cdbb93497df1b6c640b99acc76ffd38440b42f3f
aleBertazzoli <alessandro.bertazzoli@marconirovereto.it>
2024-05-02 00:37:48 +0200

    inizio progetto

---
+++ src/main/java/it/marconi/paginalogin/PaginaLoginApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.paginalogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/marconi/paginalogin/PaginaLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marconi.paginalogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
